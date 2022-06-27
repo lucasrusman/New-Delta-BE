eed0c064f7ed4e7f16ba2e0191c1dc42b2781c6b
Facundo Rusman <rusmanfacundo@gmail.com>
2022-06-27 08:19:36 -0300

    'a'

--- routes/booking.js
+++ routes/booking.js
@@ -41,9 +41,12 @@ router.post('/cancelar', async (req, res, next) => {
 //este metodo completa y asigna
 router.post('/completar', async (req, res, next) => {
   const { patente, idReserva } = req.body;
+  console.log(req.body);
   conexion.query('UPDATE reservas SET estado = 3 WHERE (auto= ? and id = ?);', [patente, idReserva], (error, rows) => {
     if (error) {
       console.log(error);
+    }else{
+      console.log(rows);
     }
     res.status(200).json({ status: 'ok' });
   });
