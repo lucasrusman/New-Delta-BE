d7aa4ffb16a17e5c1bb8609e113a070bc6d9142c
lucasrusman <rusmanlucas@gmail.com>
2022-06-29 16:01:54 -0300

    Heroku problem with login not corrected

--- models/server.js
+++ models/server.js
@@ -56,7 +56,7 @@ class Servidor{
             );
             res.setHeader(
               "Access-Control-Allow-Methods",
-              "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+              "GET, POST, PATCH, PUT, DELETE"
             );
             next();
           });
